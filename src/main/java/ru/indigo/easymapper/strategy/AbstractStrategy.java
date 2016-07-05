2d73f83997f07a4ada05004676694ea90941bda2
Nataliia Zolotovitckaia <Nataliia_Zolotovitckaia@epam.com>
2016-07-05 15:24:12 +0300

    fix

--- src/main/java/ru/indigo/easymapper/strategy/AbstractStrategy.java
+++ src/main/java/ru/indigo/easymapper/strategy/AbstractStrategy.java
@@ -2,7 +2,7 @@ package ru.indigo.easymapper.strategy;
 
 import ru.indigo.easymapper.EasyMapper;
 
-public abstract class AbstractStrategy implements Strategy {
+abstract class AbstractStrategy implements Strategy {
 
     EasyMapper EASY_MAPPER = new EasyMapper();
 }
